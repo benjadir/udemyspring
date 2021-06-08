e11acc1e2c6e0560a0f714005bcc4584176dcce0
benjadir <rida.jeba@gmail.com>
2021-06-08 10:47:17 +0100

    use application.properties to set litteral value in the applicationcontext.xml

--- src/main/java/com/udemy/cousedemo/springboot/SpringbootcourseApplication.java
+++ src/main/java/com/udemy/cousedemo/springboot/SpringbootcourseApplication.java
@@ -9,9 +9,9 @@ public class SpringbootcourseApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootcourseApplication.class, args);
-        ClassPathXmlApplicationContext ctx=new ClassPathXmlApplicationContext("applicationContext.xml");
+        ClassPathXmlApplicationContext context=new ClassPathXmlApplicationContext("applicationContext.xml");
       //  Coach coa=ctx.getBean("mycoach",Coach.class); //Coach INTERFACE CLASS BOTH WORK but there is diffrence
-        footballCoach coa2= ctx.getBean("mycoach",footballCoach.class);
+        footballCoach coa2= context.getBean("mycoach",footballCoach.class);
         System.out.println("footbal coatch mail: "+ ((footballCoach) coa2).getEmailadress());
 
        // System.out.println(coa.getDailyFortune());
